af3abdba12a044bf63eea45bd8d587c43aa908cb
Michelle Moon <michellemoon236@gmail.com>
2019-05-29 18:28:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -19,7 +19,7 @@ class App < Sinatra::Base
   end
 
   get "/multiply/:num1/:num2" do
-    binding.pry
+    #binding.pry
     @product = params[:num1].to_i*params[:num2].to_i
     "#{@product}"
   end
